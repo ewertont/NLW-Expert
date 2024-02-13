e1600a0866dff37bf7ea210ed997d39365c1a3d2
Ewerton Tavares <ewerton_tavares@live.com>
2024-02-13 16:19:52 -0300

    Aula 3

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,7 +15,7 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none flex flex-col gap-3'>
         <span className='text-sm font-medium text-slate-300'>
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
         <p className='text-sm leading-6 text-slate-400'>
           {note.content}
