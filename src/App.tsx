5cb390227e0b50eb6826efcfbcf7deaeee1bfe2f
Ewerton Tavares <ewerton_tavares@live.com>
2024-02-13 16:30:50 -0300

    delete e responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -38,10 +38,20 @@ export function App() {
     setSearch(query);
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
   const filteredNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes;
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5 md:px-0'>
       <img src={logo} alt="NLW Expert" />
 
       {/* Search Form */}
@@ -56,11 +66,11 @@ export function App() {
       <div className='h-px bg-slate-700'></div>
 
       {/* Grid Cards */}
-      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6'>
 
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+          return <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
         })}
       </div>
     </div>
